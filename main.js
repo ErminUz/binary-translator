d078f52c180d184f8177fb7e6bcddf4105e80067
macbook <uzunic.e@gmail.com>
2020-12-02 19:38:32 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function display(){
+
+}
+
+function binaryToText(text){
+
+}
+
+function binaryToDecimal(bin){
+
+}
+
+/*
+  Kanske kan fixa fram en metod som används i översättningsmetoderna och tar
+  värdet som returneras och målar upp det i formatet man vill ha; text i div eller hur
+  man nu vill ha det. Svårighetsgraden är nog för hög nu, dessutom är det kanske inte 
+  ett smart sätt. 
+*/
+function renderOutput(output){}
+
